ca363cf0ba95b9d623a0e5786b91a3e5edc25618
Silverlamb <ishaank18@gmail.com>
2020-08-28 18:31:21 -0400

    First Commit For Hello World

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request, redirect, make_response
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+	return render_template("index.html")
+
+app.run(host='0.0.0.0', port=8080)
